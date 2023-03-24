c468d998b883083f6b2d6fa4e9dbb574ed6c72a2
otiswolf <wolfotis@gmail.com>
2023-03-24 14:34:25 -0500

    Add Custom Item and Creative Mode Tab

--- src/main/java/com/otiswolf/tutorialmod/TutorialMod.java
+++ src/main/java/com/otiswolf/tutorialmod/TutorialMod.java
@@ -1,8 +1,12 @@
 package com.otiswolf.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import com.otiswolf.tutorialmod.item.ModCreativeModeTabs;
+import com.otiswolf.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,14 +25,31 @@ public class TutorialMod
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BLACK_OPAL);
+            event.accept(ModItems.RAW_BLACK_OPAL);
+        }
+
+        if (event.getTab() == ModCreativeModeTabs.TUTORIAL_TAB) {
+            event.accept(ModItems.BLACK_OPAL);
+            event.accept(ModItems.RAW_BLACK_OPAL);
+        }
 
     }
 
---
+++ src/main/java/com/otiswolf/tutorialmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package com.otiswolf.tutorialmod.item;
+
+import com.otiswolf.tutorialmod.TutorialMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> BLACK_OPAL = ITEMS.register("black_opal",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_BLACK_OPAL = ITEMS.register("raw_black_opal",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
